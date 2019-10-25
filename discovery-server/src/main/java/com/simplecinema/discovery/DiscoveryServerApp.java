d9ef2eb45a9c3296f7b01734dd5f1fb6702deda8
GaryChengld <chengld@yahoo.com>
2019-10-24 22:36:54 -0400

    docker

--- discovery-server/src/main/java/com/simplecinema/discovery/DiscoveryServerApp.java
+++ discovery-server/src/main/java/com/simplecinema/discovery/DiscoveryServerApp.java
@@ -1,4 +1,4 @@
-package com.simplecinema.discovery.simplecinemadiscoveryserver;
+package com.simplecinema.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author Gary Cheng
  */
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApp {
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApp.class, args);
