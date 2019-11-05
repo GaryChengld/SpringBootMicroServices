5f3c61e08b21c5db1734eca017e8346be3529a7a
GaryChengld <chengld@yahoo.com>
2019-11-04 19:55:44 -0500

    Auth server

---
+++ auth-server/src/test/java/com/simplecinema/auth/SimpleCinemaAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecinema.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCinemaAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
